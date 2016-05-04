7c2ba1811c18562b21cff002aeba992ae8376b6b
Marco129 <marco.website01@gmail.com>
2016-05-04 21:03:56 +0800

    Update layout

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
+'use strict';
+
 angular.module('gpa', [])
 	.controller('gpaController', function($scope, $rootScope){
 		$scope.gpa = 0;
-		$scope.default_courses = [];
 		$scope.courses = [];
 		$scope.scales = [];
 		$scope.add = function(){
 			$scope.courses.push({course: $scope.add_course, credit: $scope.add_credit, grade: $scope.add_grade});
-			$scope.add_course = $scope.add_credit = $scope.add_grade = '';
+			$scope.reset();
 			$scope.addForm.$setPristine();
 			$scope.calulateGPA();
 		}
@@ -23,16 +24,20 @@ angular.module('gpa', [])
 				$scope.gpa = 0;
 			}
 		}
+		$scope.reset = function(){
+			$scope.add_course = $scope.add_credit = $scope.add_grade = '';
+		}
 		$scope.clear = function(){
-			$scope.courses = angular.copy($scope.default_courses);
+			$scope.courses = angular.copy([]);
 			$scope.calulateGPA();
 		}
 		$scope.clear();
 		// receive sync from scaleController
 		$rootScope.$on('scale', function(event, message){
-			$scope.scales  = message;
+			$scope.scales = message;
 		});
-	}).controller('scaleController', function($scope, $rootScope){
+	})
+	.controller('scaleController', function($scope, $rootScope){
 		$scope.default_scales = [
 		                         {grade: 'A+', point: 4.5},
 				                 {grade: 'A', point: 4.0},
